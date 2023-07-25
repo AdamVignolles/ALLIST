1fbb2afa3137e9f8ff85196b2bfffd0a1a047c1b
AdamVignolles <adam.vignolles@gmail.com>
2023-07-25 11:32:44 +0200

    .

--- ALLIST/ALLIST/settings.py
+++ ALLIST/ALLIST/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ALLIST.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["ALLIST/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
